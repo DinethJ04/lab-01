ec8870d5bd26a7b739a87e2ad66bd7505a500fd0
DinethJ04 <135941410+DinethJ04@users.noreply.github.com>
2023-07-13 09:46:58 +0530

    Add files via upload

---
+++ labsession/src/main/java/com/mycompany/labsession/Labsession.java
@@ -0,0 +1,13 @@
+package com.mycompany.labsession;
+public class Labsession 
+{
+    public static void main(String args[])
+    {
+    int i=0;
+    while (i<5)
+    {
+        System.out.println("loop executing "+i);
+        i++;
+    }
+    }
+}
